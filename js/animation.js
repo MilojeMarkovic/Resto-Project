02508036ee2bbb9d5f81acfbb1cbaa3e9ec87f62
Miloje Markovic <miloje.markovic65@gmail.com>
2018-07-20 19:30:37 +0200

    new commit

--- js/animation.js
+++ js/animation.js
@@ -91,3 +91,28 @@ function showDivs(n) {
     x[slideIndex-1].style.display = "block";  
     dots[slideIndex-1].className += " active";
 }
+
+/* Diches image change */
+
+var slideIndex = 1;
+showDiv(slideIndex);
+
+function currentDivs(n) {
+  showDiv(slideIndex = n);
+}
+
+function showDiv(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("small-dots");
+    if (n > x.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = x.length}
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    x[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+}
